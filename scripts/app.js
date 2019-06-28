89a35fbba79099f595c88419220f4e1b7eed9988
Marcin Pietkiewicz <marcin.p.dev@gmail.com>
2019-06-28 17:06:43 +0200

    Added svg images for the app

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,15 @@
 const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+
 
 const updateUI = (data) => {
-    const cityDets = data.cityDets;
-    const weather = data.weather;
+
+    // destructure properties
+
+    const { cityDets, weather } = data;
 
     //update details template
     details.innerHTML = `                
