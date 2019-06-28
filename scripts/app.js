a2c33d9264002ac255e9223e967a07a3723675ef
Marcin Pietkiewicz <marcin.p.dev@gmail.com>
2019-06-28 17:54:03 +0200

    Added local storage save for last displayed city

--- scripts/app.js
+++ scripts/app.js
@@ -24,11 +24,7 @@ const updateUI = (data) => {
     const iconSrc = `img/icons/${weather.WeatherIcon}.svg`
     icon.setAttribute('src', iconSrc);
 
-
-    let timeSrc = null;
-    if (weather.IsDayTime) {
-        timeSrc = 'img/day.svg'
-    } else { timeSrc = 'img/night.svg' }
+    let timeSrc = (weather.IsDayTime) ? 'img/day.svg' : 'img/night.svg'
     time.setAttribute('src', timeSrc)
 
 
@@ -58,4 +54,14 @@ cityForm.addEventListener('submit', e => {
     updateCity(city)
         .then(data => updateUI(data))
         .catch(err => console.log(error))
-})
+
+    // set local storage
+    localStorage.setItem('city', city);
+});
+
+if (localStorage.getItem('city')) {
+    updateCity(localStorage.getItem('city'))
+        .then(data => updateUI(data))
+        .catch(err => console.log(err))
+
+}
