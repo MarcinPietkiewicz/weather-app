bcaa865e354bdf60b8c2fc9ba9c69257f5303a63
Marcin Pietkiewicz <marcin.p.dev@gmail.com>
2019-06-28 15:58:44 +0200

    Implemented query for accuWeather api for city

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,1 +1,16 @@
-console.log('forecast script')
+const key = '0AHBe3pZQpXQa1GzoKAhsFLGlynio3bx';
+
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+}
+
+getCity('Manchester')
+    .then(data => console.log(data))
+    .catch(err => HTMLFormControlsCollection.log(err))
